2136bae606e651cfe322aaf8af8f9e6c20d425df
Ruben <ruben@zkr.nl>
2024-06-14 18:43:18 +0200

    fix

--- src/api/ServiceBus.js
+++ src/api/ServiceBus.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const URL = "https://aquaversity-api.azurewebsites.net/";
+const URL = "https://aquaversity-api.azurewebsites.net";
 
 export const login = async (email, password) => {
   try {
